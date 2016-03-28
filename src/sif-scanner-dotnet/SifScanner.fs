5e3e59441635f2cd58d8c9451d18a694d70a3df7
tucaz <me@tucaz.net>
2016-03-27 21:33:53 -0500

    First version of breakBy pattern

---
+++ src/sif-scanner-dotnet/SifScanner.fs
@@ -0,0 +1,21 @@
+﻿#if INTERACTIVE
+#else
+module SifScanner 
+#endif
+
+let breakBy lines pattern =
+    let rec breakByRec lines pattern acc = 
+        match lines with 
+        | [] -> acc
+        | head::tail ->
+            match pattern head with
+            | true ->
+                let newGroup = [head]
+                let newList = newGroup :: acc
+                breakByRec tail pattern newList
+            | false ->
+                let lastGroup = List.head acc                
+                let newGroup = head :: lastGroup;
+                let newList = newGroup :: List.tail acc
+                breakByRec tail pattern newList
+    breakByRec lines pattern [[]]
---
+++ src/sif-scanner-dotnet/SifScannerTests.fs
@@ -0,0 +1,19 @@
+﻿#if INTERACTIVE
+#else
+module SifScannerTests
+open SifScanner
+#endif
+
+open System.IO
+open System.Text.RegularExpressions
+
+let data = [
+    "Name=John"; "Age=29"; "City=San Francisco";
+    "Name=Jane"; "Age=28"; "City=New York";
+    "Name=Mike"; "Age=35"; "City=Miami"
+]
+
+let matchName line = Regex.IsMatch(line, "^Name=")
+let people = breakBy data matchName
+
+printfn "%A" people
